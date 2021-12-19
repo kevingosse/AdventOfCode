ce1653d9c2c151a453b51d56ba716ab23089fac1
Kevin Gosse <krix33@gmail.com>
2021-12-19 21:29:10 +0100

    Year 2015, Day 13

---
+++ src/2015/Day13.cs
@@ -0,0 +1,100 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2015
+{
+    internal class Day13 : Problem
+    {
+        private readonly Dictionary<string, Attendee> _attendees = new();
+
+        public override void Solve()
+        {
+            foreach (var line in File.ReadLines(Input))
+            {
+                var match = Regex.Match(line, @"(?<name1>\w+) would (?<verb>\w+) (?<value>[0-9]+) happiness units by sitting next to (?<name2>\w+).");
+
+                var value = int.Parse(match.Groups["value"].Value);
+
+                if (match.Groups["verb"].Value == "lose")
+                {
+                    value *= -1;
+                }
+
+                if (!_attendees.TryGetValue(match.Groups["name1"].Value, out var attendee))
+                {
+                    attendee = new Attendee(match.Groups["name1"].Value);
+                    _attendees.Add(attendee.Name, attendee);
+                }
+
+                attendee.Relationship.Add(match.Groups["name2"].Value, value);
+            }
+
+            Console.WriteLine(ComputeBestHappiness());
+        }
+
+        private int ComputeBestHappiness()
+        {
+            int maxHappiness = int.MinValue;
+
+            foreach (var permutation in _attendees.Values.GetPermutations())
+            {
+                int happiness = 0;
+
+                for (int i = 0; i < permutation.Length; i++)
+                {
+                    Attendee attendee1;
+                    Attendee attendee2;
+
+                    if (i == 0)
+                    {
+                        attendee1 = permutation[0];
+                        attendee2 = permutation.Last();
+                    }
+                    else
+                    {
+                        attendee1 = permutation[i - 1];
+                        attendee2 = permutation[i];
+                    }
+
+                    happiness += attendee1.Relationship[attendee2.Name];
+                    happiness += attendee2.Relationship[attendee1.Name];
+                }
+
+                if (happiness > maxHappiness)
+                {
+                    maxHappiness = happiness;
+                }
+            }
+
+            return maxHappiness;
+        }
+
+        private record Attendee(string Name)
+        {
+            public Dictionary<string, int> Relationship { get; } = new Dictionary<string, int>();
+        }
+
+        public override void Solve2()
+        {
+            var myself = new Attendee("Myself");
+
+            foreach (var attendee in _attendees.Values)
+            {
+                myself.Relationship.Add(attendee.Name, 0);
+                attendee.Relationship.Add(myself.Name, 0);
+            }
+
+            _attendees.Add(myself.Name, myself);
+
+            Console.WriteLine(ComputeBestHappiness());
+        }
+    }
+}
--- src/Extensions.cs
+++ src/Extensions.cs
@@ -32,6 +32,14 @@ namespace AdventOfCode
             }
         }
 
+        public static List<T[]> GetPermutations<T>(this IEnumerable<T> source)
+        {
+            var sourceArray = source.ToArray();
+            var results = new List<T[]>();
+            Permute(sourceArray, 0, sourceArray.Length - 1, results);
+            return results;
+        }
+
         private static T TryEnumerate<T>(IEnumerator<T> enumerator)
         {
             if (!enumerator.MoveNext())
@@ -40,6 +48,29 @@ namespace AdventOfCode
             }
 
             return enumerator.Current;
+        }
+
+        private static void Permute<T>(T[] elements, int recursionDepth, int maxDepth, ICollection<T[]> results)
+        {
+            if (recursionDepth == maxDepth)
+            {
+                results.Add(elements.ToArray());
+                return;
+            }
+
+            for (var i = recursionDepth; i <= maxDepth; i++)
+            {
+                Swap(ref elements[recursionDepth], ref elements[i]);
+                Permute(elements, recursionDepth + 1, maxDepth, results);
+                Swap(ref elements[recursionDepth], ref elements[i]);
+            }
+        }
+
+        private static void Swap<T>(ref T a, ref T b)
+        {
+            T tmp = a;
+            a = b;
+            b = tmp;
         }
     }
 }
