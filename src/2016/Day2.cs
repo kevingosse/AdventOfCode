2d0a45eb24ba44512362994e6d80efca006dd669
Kevin Gosse <krix33@gmail.com>
2022-12-01 22:03:26 +0100

    Year 2016, Day 3

--- src/2016/Day2.cs
+++ src/2016/Day2.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AdventOfCode._2016
+﻿namespace AdventOfCode._2016
 {
     internal class Day2 : Problem
     {
---
+++ src/2016/Day3.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode._2016
+{
+    internal class Day3 : Problem
+    {
+        public override void Solve()
+        {
+            int count = 0;
+
+            foreach (var line in File.ReadLines(Input))
+            {
+                var (side1, side2, side3) = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Order();
+
+                if (side1 + side2 > side3)
+                {
+                    count += 1;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+
+        public override void Solve2()
+        {
+            int count = 0;
+
+            foreach (var triangle in GetTriangles())
+            {
+                var (side1, side2, side3) = triangle.Order();
+
+                if (side1 + side2 > side3)
+                {
+                    count += 1;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+
+        private IEnumerable<int[]> GetTriangles()
+        {
+            foreach (var group in File.ReadLines(Input).Select(l => l.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)).Chunk(3))
+            {
+                foreach (var elements in group[0].Zip(group[1], group[2]))
+                {
+                    yield return new[] { elements.First, elements.Second, elements.Third };
+                }
+            }
+        }
+
+    }
+}
