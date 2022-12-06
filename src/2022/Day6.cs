1b07a2baadd91d018eff72c3cb0d2079ae18fd37
Kevin Gosse <krix33@gmail.com>
2022-12-06 11:45:20 +0100

    Year 2022, Day 6

---
+++ src/2022/Day6.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode._2022
+{
+    internal class Day6 : Problem
+    {
+        public override void Solve()
+        {
+            var input = File.ReadAllText(Input);
+
+            for (int i = 0; i < input.Length - 4; i++)
+            {
+                if (input[i..(i + 4)].Distinct().Count() == 4)
+                {
+                    Console.WriteLine(i + 4);
+                    return;
+                }
+            }
+        }
+
+        public override void Solve2()
+        {
+            var input = File.ReadAllText(Input);
+
+            for (int i = 0; i < input.Length - 14; i++)
+            {
+                if (input[i..(i + 14)].Distinct().Count() == 14)
+                {
+                    Console.WriteLine(i + 14);
+                    return;
+                }
+            }
+        }
+    }
+}
