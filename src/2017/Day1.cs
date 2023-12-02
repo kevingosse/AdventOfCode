5eee5e4fc02bd09d68e3cc6629662dcabd72b974
Kevin Gosse <krix33@gmail.com>
2023-12-02 22:06:09 +0100

    Year 2017, day 1

---
+++ src/2017/Day1.cs
@@ -0,0 +1,46 @@
+﻿namespace AdventOfCode._2017;
+
+internal class Day1 : Problem
+{
+    public override void Solve()
+    {
+        long total = 0;
+
+        foreach (var line in File.ReadLines(Input))
+        {
+            for (int i = 0; i < line.Length; i++)
+            {
+                var c = line[i];
+                var next = line[(i + 1) % line.Length];
+
+                if (c == next)
+                {
+                    total += c - '0';
+                }
+            }
+        }
+
+        Console.WriteLine(total);
+    }
+
+    public override void Solve2()
+    {
+        long total = 0;
+
+        foreach (var line in File.ReadLines(Input))
+        {
+            for (int i = 0; i < line.Length; i++)
+            {
+                var c = line[i];
+                var next = line[(i + line.Length / 2) % line.Length];
+
+                if (c == next)
+                {
+                    total += c - '0';
+                }
+            }
+        }
+
+        Console.WriteLine(total);
+    }
+}
