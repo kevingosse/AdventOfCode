2f6e8847cb75c85871aaf918a6e40fc6b2189bd8
Kevin Gosse <krix33@gmail.com>
2024-12-03 18:26:32 +0100

    Year 2024, day 3

---
+++ src/2024/Day3.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode._2024;
+
+internal class Day3 : Problem
+{
+    public override void Solve()
+    {
+        var input = File.ReadAllText(Input);
+        long result = 0;
+
+        foreach (Match match in Regex.Matches(input, @"mul\((\d+),(\d+)\)"))
+        {
+            var (op1, op2) = match.As<int, int>();
+            result += op1 * op2;
+        }
+
+        Console.WriteLine(result);
+    }
+
+    public override void Solve2()
+    {
+        var input = File.ReadAllText(Input);
+        
+        long result = 0;
+        bool enabled = true;
+
+        foreach (Match match in Regex.Matches(input, @"mul\((\d+),(\d+)\)|(do\(\)|don't\(\))"))
+        {
+            if (match.Groups[3].Success)
+            {
+                enabled = match.Groups[3].Value == "do()";
+                continue;
+            }
+
+            if (!enabled)
+            {
+                continue;
+            }
+
+            var (op1, op2) = match.As<int, int>();
+            result += op1 * op2;
+        }
+
+        Console.WriteLine(result);
+    }
+}
