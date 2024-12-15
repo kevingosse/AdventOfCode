6337bd78963c26cd4f9dbd69b323746a11ff005f
Kevin Gosse <krix33@gmail.com>
2024-12-15 16:42:17 +0100

    year 2017, day 5

---
+++ src/2017/Day5.cs
@@ -0,0 +1,42 @@
+﻿namespace AdventOfCode._2017;
+
+internal class Day5 : Problem
+{
+    public override void Solve()
+    {
+        var offsets = File.ReadLines(Input).Select(int.Parse).ToArray();
+
+        int steps = 0;
+        int index = 0;
+
+        do
+        {
+            steps++;
+            ref var jump = ref offsets[index];
+            index = index + jump;
+            jump++;
+        }
+        while (index < offsets.Length && index >= 0);
+
+        Console.WriteLine(steps);
+    }
+
+    public override void Solve2()
+    {
+        var offsets = File.ReadLines(Input).Select(int.Parse).ToArray();
+
+        int steps = 0;
+        int index = 0;
+
+        do
+        {
+            steps++;
+            ref var jump = ref offsets[index];
+            index = index + jump;
+            jump += jump >= 3 ? -1 : 1;
+        }
+        while (index < offsets.Length && index >= 0);
+
+        Console.WriteLine(steps);
+    }
+}
