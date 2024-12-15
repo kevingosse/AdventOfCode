e57a5134a9359253164155a904cc853d08a91086
Kevin Gosse <krix33@gmail.com>
2024-12-15 16:32:10 +0100

    Year 2017, day 4

---
+++ src/2017/Day4.cs
@@ -0,0 +1,58 @@
+﻿namespace AdventOfCode._2017;
+
+internal class Day4 : Problem
+{
+    public override void Solve()
+    {
+        int count = 0;
+
+        foreach (var line in File.ReadLines(Input))
+        {
+            bool isValid = true;
+            var words = new HashSet<string>();
+
+            foreach (var word in line.Split(' '))
+            {
+                if (!words.Add(word))
+                {
+                    isValid = false;
+                    break;
+                }
+            }
+
+            if (isValid)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine(count);
+    }
+
+    public override void Solve2()
+    {
+        int count = 0;
+
+        foreach (var line in File.ReadLines(Input))
+        {
+            bool isValid = true;
+            var words = new HashSet<string>();
+
+            foreach (var word in line.Split(' '))
+            {
+                if (!words.Add(string.Concat(word.Order())))
+                {
+                    isValid = false;
+                    break;
+                }
+            }
+
+            if (isValid)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine(count);
+    }
+}
