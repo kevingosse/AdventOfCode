e607fa9235b26034a6cf264f9b6bdce74bcc2572
Kevin Gosse <krix33@gmail.com>
2024-12-18 19:51:57 +0100

    Year 2017, day 9

---
+++ src/2017/Day9.cs
@@ -0,0 +1,86 @@
+﻿namespace AdventOfCode._2017;
+
+internal class Day9 : Problem
+{
+    public override void Solve()
+    {
+        var input = File.ReadAllText(Input);
+
+        int nestedLevel = 0;
+        bool garbage = false;
+        int result = 0;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            var c = input[i];
+
+            if (garbage)
+            {
+                if (c == '>')
+                {
+                    garbage = false;
+                }
+                else if (c == '!')
+                {
+                    i++;
+                }
+            }
+            else
+            {
+                if (c == '<')
+                {
+                    garbage = true;
+                }
+                else if (c == '{')
+                {
+                    nestedLevel++;
+                }
+                else if (c == '}')
+                {
+                    result += nestedLevel;
+                    nestedLevel--;
+                }
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+
+    public override void Solve2()
+    {
+        var input = File.ReadAllText(Input);
+
+        bool garbage = false;
+        int result = 0;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            var c = input[i];
+
+            if (garbage)
+            {
+                if (c == '>')
+                {
+                    garbage = false;
+                }
+                else if (c == '!')
+                {
+                    i++;
+                }
+                else
+                {
+                    result++;
+                }
+            }
+            else
+            {
+                if (c == '<')
+                {
+                    garbage = true;
+                }
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+}
